ed038749ae7af71c750472bb16fc03f8397bde27
umutyerebakmaz <umutyerebakmaz@gmail.com>
2024-04-07 05:07:34 +0300

    add home component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HOME_ROUTES } from '@page/home/home.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  ...HOME_ROUTES,
+];
--- frontend/src/app/components/layout/footer/footer.component.ts
+++ frontend/src/app/components/layout/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common'
+
 
 @Component({
   standalone: true,
   selector: 'app-footer',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
