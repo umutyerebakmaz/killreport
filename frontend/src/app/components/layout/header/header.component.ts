32c1066132827517505b9c3c378f94efaeba2af2
umutyerebakmaz <umutyerebakmaz@gmail.com>
2024-04-08 17:59:28 +0300

    add menu dropdown items

--- frontend/src/app/components/layout/header/header.component.ts
+++ frontend/src/app/components/layout/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, MatDivider],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
