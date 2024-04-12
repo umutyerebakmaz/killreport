aeba42039622610de5fe6675bfe88078f8002cd3
umutyerebakmaz <umutyerebakmaz@gmail.com>
2024-04-12 15:23:03 +0300

    add header tooltips

--- frontend/src/app/components/layout/footer/footer.component.ts
+++ frontend/src/app/components/layout/footer/footer.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  standalone: true,
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    standalone: true,
+    selector: 'app-footer',
+    imports: [],
+    templateUrl: './footer.component.html'
 })
 export class FooterComponent {
 
--- frontend/src/app/components/layout/header/header.component.ts
+++ frontend/src/app/components/layout/header/header.component.ts
@@ -3,12 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDivider } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
-    selector: 'app-header',
-    standalone: true,
-    imports: [MatButtonModule, MatIconModule, MatMenuModule, MatDivider],
-    templateUrl: './header.component.html',
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, MatDivider, MatTooltipModule],
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
 
