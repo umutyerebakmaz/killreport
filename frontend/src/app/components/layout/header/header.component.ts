7e84ab5b8889485db05a5b33198e1f2a259aef3c
umutyerebakmaz <umutyerebakmaz@gmail.com>
2024-04-18 01:59:27 +0300

    add header features

--- frontend/src/app/components/layout/header/header.component.ts
+++ frontend/src/app/components/layout/header/header.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDivider } from '@angular/material/divider';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { StatusService } from '@app/services/status.service';
+import { DatePipe, DecimalPipe } from '@angular/common';
+
+
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatMenuModule, MatDivider, MatTooltipModule],
-  templateUrl: './header.component.html',
+    selector: 'app-header',
+    standalone: true,
+    imports: [MatButtonModule, MatIcon, MatMenuModule, MatDivider, MatTooltipModule, DecimalPipe, DatePipe],
+    templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    #statusService = inject(StatusService);
+    players!: number | undefined;
+    utcDate!: Date;
+
+    ngOnInit(): void {
+        this.getStatus();
+        this.utcDate = new Date();
+    }
+
+
+    getStatus(): void {
+        this.#statusService.getStatus()
+            .subscribe(data => {
+                this.players = data.players;
+                console.log(data);
+            });
+    }
 
 }
---
+++ frontend/src/app/services/status.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export type status = {
+  players?: number,
+  server_version?: string,
+  start_time?: string
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatusService {
+
+  private apiUrl = 'https://esi.evetech.net/latest/status/?datasource=tranquility';
+
+  constructor(private http: HttpClient) {}
+
+  getStatus(): Observable<status> {
+    return this.http.get<status>(this.apiUrl);
+  }
+}
