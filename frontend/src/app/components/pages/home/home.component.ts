1078351a63132be741e213d76758b8663e7adbc0
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2024-04-21 18:24:55 +0300

    add esi alliance endpoints (#54)

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AllianceService } from '../../../services/esi/alliance.service';
 
 @Component({
     standalone: true,
     selector: 'home',
     templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+    #allianceService = inject(AllianceService);
+    ngOnInit(): void {
+        this.#allianceService.getAllianceIcon(99003581).subscribe(data => {
+            console.log(data);
+        });
+    }
+}
