570077d0336724a33788854fd6ba37e99e10bbcf
umutyerebakmaz <umutyerebakmaz@gmail.com>
2024-04-21 19:46:10 +0300

    add character info card design

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { AllianceService } from '../../../services/esi/alliance.service';
+import { AllianceService } from '@service/esi/alliance.service';
+import { CharacterInfoCardComponent } from '@app/components/ui/character-info-card/character-info-card.component';
 
 @Component({
     standalone: true,
     selector: 'home',
     templateUrl: './home.component.html',
+    imports: [CharacterInfoCardComponent],
 })
 export class HomeComponent implements OnInit {
     #allianceService = inject(AllianceService);
