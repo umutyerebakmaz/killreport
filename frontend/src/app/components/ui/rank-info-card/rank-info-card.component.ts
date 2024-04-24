7f44f3521d9852a44f161bf45cd4b89ae5bc129f
umutyerebakmaz <umutyerebakmaz@gmail.com>
2024-04-24 03:47:47 +0300

    add rank table

--- frontend/src/app/components/ui/rank-info-card/rank-info-card.component.ts
+++ frontend/src/app/components/ui/rank-info-card/rank-info-card.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
-
+import { RankTableComponent } from './rank-table/rank-table.component';
 @Component({
     standalone: true,
     selector: 'rank-info-card',
     templateUrl: './rank-info-card.component.html',
-    imports: [MatButton],
+    imports: [MatButton, NgIf, NgClass, RankTableComponent],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RankInfoCardComponent implements OnInit {
-    constructor() {}
+export class RankInfoCardComponent {
+    tab: number = 1;
 
-    ngOnInit() {}
+    get getButtonClass() {
+        return {
+            'transition duration-300 ease-in-out': true,
+            'px-2 py-1 text-sm text-white rounded-md shadow-sm cursor-pointer bg-white/10 hover:bg-white/20': true,
+            'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 focus:ring-offset-black': true,
+        };
+    }
 }
---
+++ frontend/src/app/components/ui/rank-info-card/rank-table/rank-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'rank-table',
+    templateUrl: './rank-table.component.html',
+})
+export class RankTableComponent {}
