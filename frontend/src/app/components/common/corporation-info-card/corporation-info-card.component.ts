6173cb6e52a24d4d21cb41f436f80c65aae3f3a5
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2024-04-29 20:44:18 +0300

    add corporation information card and page (#68)

---
+++ frontend/src/app/components/common/corporation-info-card/corporation-info-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { MatTooltip } from '@angular/material/tooltip';
+import { Corporation } from '@app/components/page/corporation/corporation.component';
+
+@Component({
+    standalone: true,
+    selector: 'corporation-info-card',
+    templateUrl: './corporation-info-card.component.html',
+    imports: [MatTooltip]
+})
+export class CorporationInfoCardComponent {
+    @Input() corporation!: Corporation;
+}
