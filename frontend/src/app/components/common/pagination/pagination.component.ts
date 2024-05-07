6692fa0e95229ced131e02e0a47753da756c01d8
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2024-05-08 01:57:40 +0300

    add basic mat-pagniator (#77)
    Co-authored-by: “Umut <“umutyerebakmaz@users.norepyly.github.com”>

---
+++ frontend/src/app/components/common/pagination/pagination.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+@Component({
+    standalone: true,
+    selector: 'pagination',
+    templateUrl: './pagination.component.html',
+    imports: [MatPaginatorModule]
+})
+export class PaginationComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {
+    }
+
+}
--- frontend/src/app/components/page/home/home.component.ts
+++ frontend/src/app/components/page/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { KillTableComponent } from '@app/components/common/kill-table/kill-table.component';
 import { MostValuableKillsComponent } from '@app/components/common/most-valuable-kills/most-valuable-kills.component';
+import { PaginationComponent } from '@app/components/common/pagination/pagination.component';
 
 @Component({
     standalone: true,
     selector: 'home',
     templateUrl: './home.component.html',
-    imports: [MostValuableKillsComponent, KillTableComponent]
+    imports: [MostValuableKillsComponent, KillTableComponent, PaginationComponent]
 })
 export class HomeComponent implements OnInit {
     rows = [1, 2, 4, 5, 6, 7, 8, 9, 10];
