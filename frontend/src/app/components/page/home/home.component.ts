4623781735ca48a1a558c157373ddea25f86e327
Umut Yerebakmaz <umutyerebakmaz@users.norepyly.github.com>
2025-09-18 01:22:28 +0300

    refactor: update home component layout and import top characters card

--- frontend/src/app/components/page/home/home.component.ts
+++ frontend/src/app/components/page/home/home.component.ts
@@ -3,12 +3,13 @@ import { KillTableComponent } from '@ui/kill-table/kill-table.component';
 import { MostValuableKillsComponent } from '@ui/most-valuable-kills/most-valuable-kills.component';
 import { PaginationComponent } from '@ui/pagination/pagination.component';
 import { mostValuableKillsMockup } from '../../../mocks/mocks';
+import { TopCharactersCardComponent } from "@app/components/ui/top-characters-card/top-characters-card.component";
 
 @Component({
     standalone: true,
     selector: 'home',
     templateUrl: './home.component.html',
-    imports: [MostValuableKillsComponent, KillTableComponent, PaginationComponent],
+    imports: [MostValuableKillsComponent, KillTableComponent, PaginationComponent, TopCharactersCardComponent],
 })
 export class HomeComponent implements OnInit {
     rows = [1, 2, 4, 5, 6, 7, 8, 9, 10];
