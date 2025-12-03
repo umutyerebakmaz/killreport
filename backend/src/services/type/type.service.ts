e926f858a9d905d5dd67176300e71e4a66ba3adc
Umut Yerebakmaz <umutyerebakmaz@gmail.com>
2025-12-04 00:00:48 +0300

    fix: Update ESI_BASE_URL and remove unnecessary query parameters in API calls

--- backend/src/services/type/type.service.ts
+++ backend/src/services/type/type.service.ts
@@ -1,23 +1,23 @@
 import axios from 'axios';
 import { esiRateLimiter } from '../rate-limiter';
 
-const ESI_BASE_URL = 'https://esi.evetech.net/latest';
+const ESI_BASE_URL = 'https://esi.evetech.net';
 
 /**
  * Type service for ESI API interactions
  */
 export class TypeService {
-    /**
-     * Fetches type/item information (public endpoint)
-     * @param typeId - Type ID
-     * @returns Type information
-     */
-    static async getTypeInfo(typeId: number) {
-        return esiRateLimiter.execute(async () => {
-            const response = await axios.get(
-                `${ESI_BASE_URL}/universe/types/${typeId}/?datasource=tranquility`
-            );
-            return response.data;
-        });
-    }
+  /**
+   * Fetches type/item information (public endpoint)
+   * @param typeId - Type ID
+   * @returns Type information
+   */
+  static async getTypeInfo(typeId: number) {
+    return esiRateLimiter.execute(async () => {
+      const response = await axios.get(
+        `${ESI_BASE_URL}/universe/types/${typeId}/`
+      );
+      return response.data;
+    });
+  }
 }
