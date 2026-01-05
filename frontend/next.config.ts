aa135c36f67b6efa2fb131a3937b8fec4dcf479a
Umut Yerebakmaz <umutyerebakmaz@gmail.com>
2026-01-05 22:26:47 +0300

    feat: enhance link components by disabling prefetching for improved performance

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // RSC isteklerini azalt
+    staleTimes: {
+      dynamic: 30, // 30 saniye client-side cache
+      static: 180, // 3 dakika static cache
+    },
+  },
 };
 
 export default nextConfig;
