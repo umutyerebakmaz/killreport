c063d9728c32f1ade2e4115b0cb9f4dfafa7df90
Umut Yerebakmaz <umutyerebakmaz@gmail.com>
2026-03-12 21:41:07 +0300

    feat: update loader component to use lottie-react; enhance animation handling and improve package dependencies

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,18 +1,23 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: '../backend/src/generated-schema.graphql',
-    documents: ['src/**/*.graphql'],
-    ignoreNoDocuments: true,
-    generates: {
-        './src/generated/graphql.ts': {
-            plugins: [
-                'typescript',
-                'typescript-operations',
-                'typescript-react-apollo',
-            ],
-        },
+  schema: '../backend/src/generated-schema.graphql',
+  documents: ['src/**/*.graphql'],
+  ignoreNoDocuments: true,
+  generates: {
+    './src/generated/graphql.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      config: {
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+      },
     },
+  },
 };
 
 export default config;
